c99e3ae79bbba1eb8ca635c2a2dc56a2cf45cbc7
teju <tejutanvi773@gmail.com>
2020-07-16 10:20:47 +0530

    Added : Hello world Example

---
+++ src/Demo.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+class Demo extends Component{
+render(){
+return  <h1> Hello World </h1>
+}
+
+
+
+}
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Demo from './Demo';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Demo />
   </React.StrictMode>,
   document.getElementById('root')
 );
